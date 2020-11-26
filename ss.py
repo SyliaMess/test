ea74bde08ecf9ceb0715e01deedd369e76e483a1
SyliaMess <75087510+SyliaMess@users.noreply.github.com>
2020-11-26 19:31:20 +0100

    Add files via upload

--- ss.py
+++ ss.py
@@ -2,15 +2,15 @@ import pyodbc
 
 
 conn = pyodbc.connect('Driver={SQL Server};'
-                      'Server=DESKTOP-BPRTOOF\SQLEXPRESS;'
-                      'Database=Gestion_de_personnel;'
+                      'Server=DESKTOP-GE0IH3H;'
+                      'Database=BDD;'
                       'Trusted_Connection=yes;')
 
 
 cursor = conn.cursor()
-cursor.execute("select * from Module;")
+cursor.execute("select * from  Bibliotheque")
 for row in cursor:
     print(row)
-    
-cursor.execute("INSERT INTO Module VALUES (('deeplearning'),('3'),('2'),('1'));")
+cursor.execute("insert into Bibliotheque values ('7','11 AVENUE JACQUE','0665432')")
 conn.commit()
+    
